8628db8b3a75cf2b8c6d7ff95f389dc66597a66e
Sahilgggg <sahilgggfdcbu@gmail.com>
2026-06-09 09:48:44 +0530

    debug

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -105,7 +105,7 @@ exports.registerUser = async (req, res) => {
         try {
           const transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
-            port: 465,
+            port: 587,
             secure: true, // true for port 465, false for other ports
             auth: {
               user: process.env.EMAIL_USER,
